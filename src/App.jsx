20d4d1d821bd5d662df15557da16b86f37d2b429
Marcos Pasacantando <marcos.pasacantando1@gmail.com>
2025-02-17 17:53:07 -0300

    NavBar y CartWidget

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import ResponsiveAppBar from './components/navbar/NavBar'
+function App() {
+  return (
+    <>
+      <ResponsiveAppBar/>
+    </>
+  )
+}
+
+export default App
